ea1673b19bca7e46a9bcffdf4ffe1d3e90cc7555
Rayanne Barros <rayanneb126@gmail.com>
2021-05-28 02:24:59 -0300

    Começando as mudanças na pag 2

--- Alura_challenge/js/menuHamburguer.js
+++ Alura_challenge/js/menuHamburguer.js
@@ -3,30 +3,3 @@ function menuOnClick() {
     document.querySelector('.lista__menu_hamburguer').classList.toggle('change');
     document.querySelector('.lista__menu_hamburguer').classList.toggle('change__hamburguer');
 }
-
-// function toggleMenu() {
-//         // if (event.type === 'touchstart') event.preventDefault();
-//         const menu = document.querySelector('.lista__menu_hamburguer');
-//         menu.classList.toggle('active');
-//     }
-    
-//     bthamburguer.addEventListener('click', toggleMenu);
-
-    
-
-    //bthamburguer.addEventListener('touchstart', toggleMenu);
-
-
-
-
-
-        // const btHamburguer = document.querySelector("#menu__botao");
-        
-        // btHamburguer.addEventListener('click', () => {
-            
-            
-        //     const menu = document.querySelector(".lista__menu_hamburguer");
-        //         menu.classList.toggle('menu__open');
-        //         btHamburguer.classList.toggle('overflow');
-        //     })
-    
