9ae4a8023c72d26db9e4c63e9f4cf69fbe97ad70
Rayanne Barros <rayanneb126@gmail.com>
2021-05-28 00:40:15 -0300

    Menu Hamburguer funcionando

---
+++ Alura_challenge/js/menuHamburguer.js
@@ -0,0 +1,32 @@
+function menuOnClick() {
+    document.getElementById('menu__botao').classList.toggle('change');
+    document.querySelector('.lista__menu_hamburguer').classList.toggle('change');
+    document.querySelector('.lista__menu_hamburguer').classList.toggle('change__hamburguer');
+}
+
+// function toggleMenu() {
+//         // if (event.type === 'touchstart') event.preventDefault();
+//         const menu = document.querySelector('.lista__menu_hamburguer');
+//         menu.classList.toggle('active');
+//     }
+    
+//     bthamburguer.addEventListener('click', toggleMenu);
+
+    
+
+    //bthamburguer.addEventListener('touchstart', toggleMenu);
+
+
+
+
+
+        // const btHamburguer = document.querySelector("#menu__botao");
+        
+        // btHamburguer.addEventListener('click', () => {
+            
+            
+        //     const menu = document.querySelector(".lista__menu_hamburguer");
+        //         menu.classList.toggle('menu__open');
+        //         btHamburguer.classList.toggle('overflow');
+        //     })
+    
