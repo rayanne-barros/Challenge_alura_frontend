7ec552db45293dbd0bb780b7af40c5db9f2ca2f8
Rayanne Barros <rayanneb126@gmail.com>
2021-05-30 00:37:46 -0300

    2° semana finalizada

--- Alura_challenge/js/menuHamburguer.js
+++ Alura_challenge/js/menuHamburguer.js
@@ -3,3 +3,18 @@ function menuOnClick() {
     document.querySelector('.lista__menu_hamburguer').classList.toggle('change');
     document.querySelector('.lista__menu_hamburguer').classList.toggle('change__hamburguer');
 }
+
+var esconder = document.querySelector(".lupa_img");
+
+esconder.addEventListener("click", function() {
+
+    var pesquisa = document.querySelector(".cabecalho__nav_pesquisa");
+    
+  if(pesquisa.style.display === "none") {
+        pesquisa.style.display = "flex";
+    } else {
+      pesquisa.style.display = "none";
+  }
+    
+});
+
