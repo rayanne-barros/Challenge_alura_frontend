76c8874e15c37bcde2635a10803555e38fb80d08
Rayanne Barros <rayanneb126@gmail.com>
2021-06-02 21:43:53 -0300

    Local Storage

--- Alura_challenge/sintaxe.js
+++ Alura_challenge/sintaxe.js
@@ -1,13 +1,15 @@
 const areaCodigo = document.querySelector('.area');
-
 const linguaguem = document.querySelector('#seletor');
-
 const botao = document.querySelector('.visualizar');
+const tituloProjeto = document.querySelector('.nome__projeto');
+const descricaoProjeto = document.querySelector('#descricao__projeto');
+const salvaProjeto= document.querySelector('.salva__projeto');
+
 
 function mudaLinguagem() {
-    const codigo = areaCodigo.querySelector('code');
+    const codigo = areaCodigo.querySelector('code').innerText;
     areaCodigo.innerHTML = `<code class="preview hljs ${linguaguem.value}" contenteditable="true" aria-label="editor"></code>`;
-    areaCodigo.firstChild.innerText = codigo.innerText;
+    areaCodigo.firstChild.innerText = codigo;
 }
 
 linguaguem.addEventListener('change', () => {
@@ -17,6 +19,52 @@ linguaguem.addEventListener('change', () => {
 botao.addEventListener('click', ()=> {
     const codigo = areaCodigo.querySelector('code');
     hljs.highlightBlock(codigo);
+});
+
+salvaProjeto.addEventListener('click', () => {
+    if (typeof(Storage) !== "undefined") {
+        console.log('Yay, support!');
+        const projeto = montaProjeto();
+        salvaLocalStorage(projeto);
+    } else {
+        console.log('Nay, no support!');
+    }
 })
+
+function montaProjeto() {
+    let projeto = {
+        'id': atribuiId(),
+        'detalhesDoProjeto': {
+            'nomeDoProjeto': tituloProjeto.value,
+            'descricaoDoProjeto': descricaoProjeto.value,
+            'linguagem': linguaguem.value,
+            'codigo': areaCodigo.querySelector('code').innerText
+        }
+    }
+    return projeto
+}
+
+let numeroId = 1
+
+if(localStorage.length > 0) {
+    numeroId = localStorage.length;
+}
+
+function atribuiId() {
+    if(localStorage.length == 0) {
+        return 0
+    } else {
+        if(localStorage.length == numeroId) {
+            let novoId = numeroId;
+            numeroId++;
+            return novoId;
+        }
+    }
+}
+
+function salvaLocalStorage(objetoJson) {
+    localStorage.setItem(objetoJson.id, JSON.stringify(objetoJson));
+}
+
 
 
